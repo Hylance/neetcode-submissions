f4ca605e0823c25286baf0d689ad3a2942eed314
Hylance <hengxingtianxia@gmail.com>
2026-07-24 15:02:06 +0800

    Add: continuous-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        reminder = {0 : -1}
+        total = 0
+        for i, num in enumerate(nums):
+            total += num
+            r = total % k
+            if r not in reminder:
+                reminder[r] = i
+            else:
+                if i - reminder[r] > 1:
+                    return True
+        return False
+        
